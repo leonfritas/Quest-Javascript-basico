413568cf1794993494dcf1da0a461651316b5b71
Leonardo Ribeiro <leon_fritas@hotmail.com>
2022-06-23 12:06:09 -0400

    quest js basico

---
+++ index.js
@@ -0,0 +1,26 @@
+function soma(numero1, numero2){
+    let soma = numero1 + numero2
+    console.log('A soma dos dois números é:' + soma)
+  
+}
+soma(1, 3)
+
+function subtracao( numero1, numero2){
+    let subtracao = numero1 - numero2
+    console.log('A subtração dos dois números é:' + subtracao)
+
+}
+subtracao(5, 3)
+
+function multiplicacao( numero1 , numero2){
+    let multiplicacao = numero1 * numero2
+    console.log('A multiplicação dos dois números é:' + multiplicacao)
+
+}
+multiplicacao(2, 2)
+
+function divisao(numero1 , numero2){
+    let divisao = numero1 / numero2
+    console.log('A divisão dos dois números é:' + divisao)
+}
+divisao(4, 2)
